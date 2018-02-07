6f727311796d8701801bc7d07883ae9c73c09459
Alexander Gunin <guninalexander@gmail.com>
2018-02-07 19:11:32 +0300

    change tf version

--- src/optimize.py
+++ src/optimize.py
@@ -179,97 +179,94 @@ def hororovod(cluster,task_index,limit,file_pattern, style_target, content_weigh
         checkpoint_dir = None
         is_chief = False
         log_step_count_steps = None
-    with tf.device(
-            tf.train.replica_device_setter(
-                worker_device=worker_device,
-                #ps_device="/job:ps",
-                cluster=cluster)):
-        dataset = styles_data(file_pattern,batch_size,limit,True)
-        num_examples = dataset['size']
-        num_samples = num_examples / batch_size
-        num_global =  num_samples * epochs
-        print("Number of iterations %d" % num_global)
-        global_step =tf.train.get_or_create_global_step()
-        X_content = tf.placeholder(tf.float32, shape=batch_shape, name="X_content")
-        X_pre = vgg.preprocess(X_content)
 
-        # precompute content features
-        content_features = {}
-        content_net = vgg.net(vgg_path, X_pre)
-        content_features[CONTENT_LAYER] = content_net[CONTENT_LAYER]
+    dataset = styles_data(file_pattern,batch_size,limit,True)
+    num_examples = dataset['size']
+    num_samples = num_examples / batch_size
+    num_global =  num_samples * epochs
+    print("Number of iterations %d" % num_global)
+    global_step =tf.train.get_or_create_global_step()
+    X_content = tf.placeholder(tf.float32, shape=batch_shape, name="X_content")
+    X_pre = vgg.preprocess(X_content)
 
-        preds = transform.net(X_content/255.0)
-        preds_pre = vgg.preprocess(preds)
+    # precompute content features
+    content_features = {}
+    content_net = vgg.net(vgg_path, X_pre)
+    content_features[CONTENT_LAYER] = content_net[CONTENT_LAYER]
 
-        net = vgg.net(vgg_path, preds_pre)
+    preds = transform.net(X_content/255.0)
+    preds_pre = vgg.preprocess(preds)
 
-        content_size = _tensor_size(content_features[CONTENT_LAYER])*batch_size
-        assert _tensor_size(content_features[CONTENT_LAYER]) == _tensor_size(net[CONTENT_LAYER])
-        content_loss = content_weight * (2 * tf.nn.l2_loss(
-            net[CONTENT_LAYER] - content_features[CONTENT_LAYER]) / content_size
-                                         )
+    net = vgg.net(vgg_path, preds_pre)
 
-        style_losses = []
-        for style_layer in STYLE_LAYERS:
-            layer = net[style_layer]
-            bs, height, width, filters = map(lambda i:i.value,layer.get_shape())
-            size = height * width * filters
-            feats = tf.reshape(layer, (bs, height * width, filters))
-            feats_T = tf.transpose(feats, perm=[0,2,1])
-            grams = tf.matmul(feats_T, feats) / size
-            style_gram = style_features[style_layer]
-            style_losses.append(2 * tf.nn.l2_loss(grams - style_gram)/style_gram.size)
+    content_size = _tensor_size(content_features[CONTENT_LAYER])*batch_size
+    assert _tensor_size(content_features[CONTENT_LAYER]) == _tensor_size(net[CONTENT_LAYER])
+    content_loss = content_weight * (2 * tf.nn.l2_loss(
+        net[CONTENT_LAYER] - content_features[CONTENT_LAYER]) / content_size
+                                     )
 
-        style_loss = style_weight * functools.reduce(tf.add, style_losses) / batch_size
+    style_losses = []
+    for style_layer in STYLE_LAYERS:
+        layer = net[style_layer]
+        bs, height, width, filters = map(lambda i:i.value,layer.get_shape())
+        size = height * width * filters
+        feats = tf.reshape(layer, (bs, height * width, filters))
+        feats_T = tf.transpose(feats, perm=[0,2,1])
+        grams = tf.matmul(feats_T, feats) / size
+        style_gram = style_features[style_layer]
+        style_losses.append(2 * tf.nn.l2_loss(grams - style_gram)/style_gram.size)
 
-        # total variation denoising
-        tv_y_size = _tensor_size(preds[:,1:,:,:])
-        tv_x_size = _tensor_size(preds[:,:,1:,:])
-        y_tv = tf.nn.l2_loss(preds[:,1:,:,:] - preds[:,:batch_shape[1]-1,:,:])
-        x_tv = tf.nn.l2_loss(preds[:,:,1:,:] - preds[:,:,:batch_shape[2]-1,:])
-        tv_loss = tv_weight*2*(x_tv/tv_x_size + y_tv/tv_y_size)/batch_size
+    style_loss = style_weight * functools.reduce(tf.add, style_losses) / batch_size
+
+    # total variation denoising
+    tv_y_size = _tensor_size(preds[:,1:,:,:])
+    tv_x_size = _tensor_size(preds[:,:,1:,:])
+    y_tv = tf.nn.l2_loss(preds[:,1:,:,:] - preds[:,:batch_shape[1]-1,:,:])
+    x_tv = tf.nn.l2_loss(preds[:,:,1:,:] - preds[:,:,:batch_shape[2]-1,:])
+    tv_loss = tv_weight*2*(x_tv/tv_x_size + y_tv/tv_y_size)/batch_size
 
 
-        loss = content_loss + style_loss + tv_loss
+    loss = content_loss + style_loss + tv_loss
 
-        tf.summary.scalar('loss', loss)
-        tf.summary.scalar('tv_loss', tv_loss)
-        tf.summary.scalar('style_loss', style_loss)
-        tf.summary.scalar('content_loss', content_loss)
-        result = preds*255.0
-        tf.summary.image('result', result,max_outputs=1)
+    tf.summary.scalar('loss', loss)
+    tf.summary.scalar('tv_loss', tv_loss)
+    tf.summary.scalar('style_loss', style_loss)
+    tf.summary.scalar('content_loss', content_loss)
+    result = preds*255.0
+    tf.summary.image('result', result,max_outputs=1)
 
-        # overall loss
-        train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss,global_step=global_step)
+    # overall loss
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss,global_step=global_step)
 
-        all_summary = tf.summary.merge_all()
+    all_summary = tf.summary.merge_all()
 
 
-        scaffold = tf.train.Scaffold(init_op=tf.global_variables_initializer(),
-                                     summary_op=all_summary)
-        scaffold.global_step = global_step
-        step = 0
-        with tf.train.MonitoredTrainingSession(master=server.target,
-                                               is_chief=is_chief,checkpoint_dir=save_path,
-                                               config=sess_config,
-                                               save_summaries_steps=10,
-                                               log_step_count_steps=10,
-                                               scaffold=scaffold) as sess:
-            while step < num_global and not sess.should_stop():
-                X_batch, _ = sess.run(dataset['batch'])
-                feed_dict = {
-                    X_content:X_batch
-                }
-                _, step = sess.run([train_step, global_step], feed_dict=feed_dict)
-                local_step += 1
-                print("Worker %d: training step %d done (global step: %d)" %
-                      (task_index, local_step, step))
-            time_end = time.time()
-            print("Training ends @ %f" % time_end)
-            training_time = time_end - time_begin
-            print("Training elapsed time: %f s" % training_time)
-            sess.request_stop()
-        return
+    scaffold = tf.train.Scaffold(init_op=tf.global_variables_initializer(),
+                                 summary_op=all_summary)
+    scaffold.global_step = global_step
+    step = 0
+    with tf.train.MonitoredTrainingSession(master=server.target,
+                                           is_chief=is_chief,checkpoint_dir=checkpoint_dir,
+                                           config=sess_config,
+                                           save_summaries_steps=log_step_count_steps,
+                                           hooks=hooks,
+                                           log_step_count_steps=log_step_count_steps,
+                                           scaffold=scaffold) as sess:
+        while step < num_global and not sess.should_stop():
+            X_batch, _ = sess.run(dataset['batch'])
+            feed_dict = {
+                X_content:X_batch
+            }
+            _, step = sess.run([train_step, global_step], feed_dict=feed_dict)
+            local_step += 1
+            print("Worker %d: training step %d done (global step: %d)" %
+                  (task_index, local_step, step))
+        time_end = time.time()
+        print("Training ends @ %f" % time_end)
+        training_time = time_end - time_begin
+        print("Training elapsed time: %f s" % training_time)
+        sess.request_stop()
+    return
 
 
 def _tensor_size(tensor):
