111f408add74dec8d82b67a9b2f01f7eb8e31a28
Alexander Gunin <guninalexander@gmail.com>
2018-04-20 16:08:06 +0300

    new template

--- src/export.py
+++ src/export.py
@@ -31,12 +31,15 @@ def export2(checkpoint_dir,export_path):
         image_array.set_shape([None,None,3])
         images = tf.image.resize_image_with_crop_or_pad([image_array],512,512)
         images = tf.to_float(images, name='ToFloat')
-
+        images = tf.reshape(images, (1,512,512,3))
         preds = transform.net(images/255.0)
 
-        result = images/255.0*255.0
+        result = preds*255.0
         result = tf.cast(result, tf.uint8)
         result_image = tf.image.encode_png(result[0])
+
+        saver = tf.train.Saver()
+        saver.restore(sess,tf.train.latest_checkpoint(checkpoint_dir))
 
         tensor_info_image = tf.saved_model.utils.build_tensor_info(image)
         tensor_info_result = tf.saved_model.utils.build_tensor_info(result_image)
@@ -45,8 +48,6 @@ def export2(checkpoint_dir,export_path):
                 inputs={'images': tensor_info_image},
                 outputs={'result': tensor_info_result},
                 method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME))
-        saver = tf.train.Saver()
-        saver.restore(sess,tf.train.latest_checkpoint(checkpoint_dir))
         builder = tf.saved_model.builder.SavedModelBuilder(export_path)
         builder.add_meta_graph_and_variables(
             sess, [tf.saved_model.tag_constants.SERVING],
